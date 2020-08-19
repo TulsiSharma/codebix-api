242bdc78cc7f44d9a2928b03c34ff9b193605df5
TusiSharma <tulsisharma449922@gmail.com>
2020-08-19 15:53:53 +0530

    upgrade

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const serverless = require("serverless-http");
-var cors = require("cors");
-var axios=require("axios");
+const cors = require("cors");
+const axios=require("axios");
 const app = express();
 const router = express.Router();
 app.use(cors());  
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 router.post("/testAPI", (req, res) => {
     var data="";
